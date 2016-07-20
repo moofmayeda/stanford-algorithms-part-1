8f46d26447897310df16d8b560722b1569d32de7
moof mayeda <moofmayeda@yahoo.com>
2016-07-20 10:46:07 -0700

    Get quicksort working when choosing first item of the array as the pivot

---
+++ week_2_practice.rb
@@ -0,0 +1,51 @@
+def binary_search(array, number)
+  length = array.length
+  half = length/2
+  if number == array[half]
+    return true
+  elsif length > 1
+    if number > array[half]
+      binary_search(array[half..length-1], number)
+    else
+      binary_search(array[0..half-1], number)
+    end
+  else
+    return false
+  end
+end
+
+def partition(array, l, r)
+  pivot_value = array[l]
+  i = l + 1
+  for j in (l+1)..r
+    if array[j] < pivot_value
+      array[i], array[j] = array[j], array[i]
+      i += 1
+    end
+  end
+  array[l], array[i-1] = array[i-1], array[l]
+end
+
+def choose_pivot(array, l, r)
+  array[l]
+end
+
+def get_pivot_index(array, pivot_value)
+  array.each_with_index do |item, i|
+    return i if array[i] == pivot_value
+  end
+end
+
+def quicksort(array, l, r)
+  length = r - l + 1
+  if length > 1
+    pivot_value = choose_pivot(array, l, r)
+    partition(array, l, r)
+    pivot_index = array.index(pivot_value)
+    quicksort(array, l, pivot_index - 1)
+    quicksort(array, pivot_index + 1, r)
+  else
+    array
+  end
+end
+quicksort([5,3,2,1,6,0], 0, 5)
