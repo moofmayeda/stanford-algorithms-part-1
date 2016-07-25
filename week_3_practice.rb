6c1476b82b03ac0e65f4957cbe8ef7049d7a701e
moof mayeda <moofmayeda@yahoo.com>
2016-07-25 14:40:59 -0700

    Deterministically choose a pivot

--- week_3_practice.rb
+++ week_3_practice.rb
@@ -10,9 +10,9 @@ def partition(array, l, r)
   array[l], array[i-1] = array[i-1], array[l]
 end
 
+# randomly choose pivot
 def choose_pivot(array, l, r)
   index = l + rand(r - l + 1)
-  array[l], array[index] = array[index], array[l]
   array[l]
 end
 
@@ -20,6 +20,8 @@ def r_select(array, i, l, r)
   length = r - l + 1
   if length > 1
     pivot_value = choose_pivot(array, l, r)
+    pivot_index = array.index(pivot_value)
+    array[l], array[pivot_index] = array[pivot_index], array[l]
     partition(array, l, r)
     pivot_index = array.index(pivot_value)
     if pivot_index + 1 == i
@@ -34,3 +36,18 @@ def r_select(array, i, l, r)
   end
 end
 r_select([5,3,2,1,6,0],3,0, 5) # 2
+
+# deterministic choose_pivot
+def choose_pivot(array, l, r)
+  middle_elements = []
+  array[l..r].each_slice(5) do |slice|
+    slice.sort!
+    if slice.length.even?
+      middle_elements << slice[slice.length / 2 - 1]
+    else
+      middle_elements << slice[slice.length / 2]
+    end
+  end
+  i = middle_elements.length.even? ? middle_elements.length / 2 : middle_elements.length / 2 + 1
+  r_select(middle_elements, i, 0, middle_elements.length - 1)
+end
