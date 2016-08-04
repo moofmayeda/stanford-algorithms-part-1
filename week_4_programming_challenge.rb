62b4df93f5839f76f67bc4e8a46f782fb29a9950
moofmayeda <moofmayeda@yahoo.com>
2016-08-03 23:11:52 -0700

    Successfully calculate largest SCCs without using recursion

--- week_4_programming_challenge.rb
+++ week_4_programming_challenge.rb
@@ -1,10 +1,10 @@
-total_lines = `wc -l "example.txt"`.strip.split(' ')[0].to_f
+total_lines = `wc -l "SCC.txt"`.strip.split(' ')[0].to_f
 
 graph_array = []
-File.open('example.txt').each do |line|
+File.open('SCC.txt').each do |line|
   graph_array << line.split(" ").map(&:to_i)
-  puts "#{$.}/#{total_lines} (#{$./total_lines * 100}%"
 end
+$n = graph_array.flatten.max
 graph = {}
 reversed_graph = {}
 graph_array.each do |array|
@@ -25,7 +25,7 @@ def dfs_loop(graph)
   $s = nil
   $new_order = $new_order || []
   if $new_order.empty?
-    (graph.count).downto(1) do |i|
+    ($n).downto(1) do |i|
       explore_vertex(graph, i)
     end
   else
@@ -38,49 +38,35 @@ def dfs_loop(graph)
   counts_hash.sort_by{|k,v| v}.reverse
 end
 
-# turned into a while loop
-# def dfs(graph, start_vertex)
-#   graph[start_vertex][:explored] = true
-#   graph[start_vertex][:leader] = $s
-#   graph[start_vertex][:data].each do |j|
-#     add_vertex(graph, j)
-#     dfs(graph, j) if !graph[j][:explored]
-#   end
-#   $t += 1
-#   graph[start_vertex][:finishing] = $t
-#   $new_order.unshift(start_vertex)
-# end
-
 def explore_vertex(graph, i)
   add_vertex(graph, i)
   if !graph[i][:explored]
-    puts "*" * 50
-    puts "vertex #{i} is about to be explored"
     $s = i
-    # dfs(graph, i)
     stack = [i]
     stack_index = 0
     while !stack[stack_index].nil?
+      stack_length = stack.count
       start_vertex = stack[stack_index]
-      # insert dfs functionality
       add_vertex(graph, start_vertex)
       graph[start_vertex][:explored] = true
       graph[start_vertex][:leader] = $s
-      puts "vertex #{start_vertex} connected to #{graph[start_vertex][:data].inspect}"
       graph[start_vertex][:data].each do |j|
         add_vertex(graph, j)
-        stack << j if !graph[j][:explored]
+        stack.insert(stack_index + 1, j) if !graph[j][:explored]
       end
-      # are all the descendents (not just the first layer) explored?
-      if stack.all? {|vertex| graph[vertex][:explored]}
+      if stack.count == stack_length
         $t += 1
         graph[start_vertex][:finishing] = $t
         $new_order.unshift(start_vertex)
-        puts "done exploring vertex #{start_vertex}, finishing order set to #{$t} and added to beginning of new_order"
-      else
-        stack << start_vertex
       end
       stack_index += 1
+    end
+    stack.reverse.each do |vertex|
+      if graph[vertex][:finishing].nil?
+        $t += 1
+        graph[vertex][:finishing] = $t
+        $new_order.unshift(vertex)
+      end
     end
   end
 end
